cbb95502dfaa9ccefb8ec2fc34bf9e16d93a873b
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-08-24 22:40:26 +0200

    switched from yarn to npm due to issue with corepack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,41 @@
+import Image from 'next/image';
+import wavyAnimation from '@/assets/gifs/wavyAnimation.webp';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <section className="flex min-h-[100vh] flex-col items-center justify-center gap-5 p-6">
-        <p className="text-2xl text-gray-500">
-          High yield, no minimum & decentralized design
-        </p>
-        <h1 className="max-w-5xl text-center text-8xl font-black uppercase text-stone-800 [text-wrap:balance]">
-          Earn{" "}
-          <span className="bg-gradient-to-r from-[#0EA5FA] to-[#045989] bg-clip-text text-transparent">
-            Trustless{" "}
-          </span>{" "}
-          Yeild.
-        </h1>
-        <button className="min-w-[100px] rounded-full bg-gradient-to-r from-[#0EA5FA] to-[#045989] px-6 py-2 text-xl text-white">
-          Launch Dapp
-        </button>
-      </section>
-      <section className="w-full p-6">
-        <div className="mx-auto w-full max-w-7xl">
-          <div className="flex h-[calc(100vh-85px)] w-full items-center justify-between gap-4 rounded-3xl bg-slate-100 pl-6">
-            <div className="flex w-[40%] flex-col gap-2">
-              <h2 className=" text-4xl font-bold text-gray-900 ">
-                No middel men.
-                <br />
-                Just <span className="text-[#0EA5FA]">code.</span>
-              </h2>
-              <p className=" text-lg text-gray-500">
-                ExtraHex is a decentralized protocol that allows users to
-                deposit and earn interest on their assets.{" "}
-                <a className="text-gray-700 underline">Learn more</a>
-              </p>
-            </div>
-            <div className=" h-[50%] w-[50%] rounded-s-xl  bg-black p-4">
-              <pre className="flex h-full items-center whitespace-pre font-mono text-white">
-                <code>
-                  <span className="text-purple-400">function</span>
-                  <span className="text-blue-300"> _stakeUnlock</span>
-                  <span className="text-yellow-400"> (</span>
-                  <span className="text-gray-100">
-                    GlobalsCache memory g, StakeCache memory st
-                  </span>
-                  <span className="text-yellow-400"> )</span>
-                  <div className="ml-6 mt-2 text-gray-100">
-                    private
-                    <br />
-                    pure
-                  </div>
-                  <div className="whitespace-normal text-yellow-400">{"{"}</div>
-                  <div className="ml-6 mt-2 text-gray-100">
-                    g._stakeSharesTotal -= st._stakeShares;
-                    <br />
-                    st._unlockedDay = g._currentDay;
-                  </div>
-                  <div className="whitespace-normal text-yellow-400">{"}"}</div>
-                </code>
-              </pre>
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <section>
+        <div className="overflow-hidden relative flex gap-4 items-center rounded-[40px] max-w-7xl w-screen bg-slate-400 h-[100vh]">
+          <div className="px-20 py-10 bg-gradient-to-br from-green-600 to-cyan-500/40 backdrop-blur-[5px] flex flex-col gap-4 justify-center w-full h-full z-10">
+            <h1 className="text-6xl font-bold max-w-2xl [text-wrap:balance] text-white">
+              Earn Descentralized Yeild. All onchain
+            </h1>
+            <h5 className="text-2xl max-w-3xl text-gray-300">
+              Hex is a descentralized smart contract where you can lock up your coins, and mint your own rewards.{' '}
+            </h5>
+            <div className="w-full flex gap-2">
+              <Button variant="default" className=" w-full max-w-[200px]">
+                Open dapp
+              </Button>
+              <Button variant="secondary" className="w-full max-w-[200px]">
+                Read more
+              </Button>
             </div>
           </div>
+          <Image src={wavyAnimation} alt="wavy animation" className="w-full h-full object-cover z-0 absolute" />
+        </div>
+      </section>
+      <section className="flex gap-4 items-center justify-center w-full mt-20 h-screen text-white">
+        <div className="flex flex-col gap-3 max-w-lg">
+          <p>1390d of flawless operation</p>
+          <h2 className="text-6xl font-bold">HEX Some cool stuff about it so this heading is nice</h2>
+          <Link href={''}> Read more {'->'} </Link>
+        </div>
+        <div className="flex flex-col gap-3 max-w-lg">
+          <h2 className="text-6xl font-bold">500k</h2>
+          <div> Read more {'->'} </div>
         </div>
       </section>
     </main>
