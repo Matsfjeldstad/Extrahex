176fe1dbc5c2e71e1567c7042fdc24a851c1b555
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-08-28 22:45:07 +0200

    adds layout to mvp of landing page

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "/tailwind.config.ts",
+  tailwindConfig: "./tailwind.config.ts",
   semi: true,
   tabWidth: 2,
   printWidth: 100,
