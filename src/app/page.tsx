fb8153156b587ccf2779f389f10110534ab5f10a
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-08-29 22:31:00 +0200

    adds extrahex logo to comming soon section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ import luckyLogo from '@/assets/photos/logos/lucky.png';
 import texanLogo from '@/assets/photos/logos/texan.png';
 import { cn } from '@/lib/utils';
 import { Arrow, EthLogo, PlsLogo } from '@/assets/icons/Icons';
+import { ExtraHexLogo } from '@/assets/photos/logos/Logos';
 
 type Ecosystem = {
   name: string;
@@ -285,22 +286,23 @@ export default function Home() {
       </section>
       <section className='my-20'>
         <div className='relative flex h-[90vh] w-screen max-w-7xl items-center gap-4 overflow-hidden rounded-[40px] bg-slate-400'>
-          <div className='z-10 flex items-center text-center h-full w-full flex-col justify-center gap-4 bg-gradient-to-br from-gray-950 to-gray-800/90 px-20 py-10 backdrop-blur-[5px]'>
-            <h1 className='max-w-2xl text-6xl leading-snug font-bold text-white [text-wrap:balance]'>
-              Mine Hex with ExtraHex
+          <div className='z-10 flex h-full w-full flex-col items-center justify-center gap-4 bg-gradient-to-br from-gray-950 to-gray-800/90 px-20 py-10 text-center backdrop-blur-[5px]'>
+            <h1 className='flex max-w-2xl flex-col items-center text-center text-3xl font-bold leading-snug text-white [text-wrap:balance]'>
+              Mine Hex with{' '}
+              <span>
+                <ExtraHexLogo className='h-[100px] fill-white' />{' '}
+              </span>
             </h1>
             <h5 className='max-w-3xl text-2xl leading-normal  text-gray-300'>
               Mine your Hex through ExtraHex, our custom front-end platform. Enjoy a seamless,
               user-friendly experience while when mining your returns.
             </h5>
-            <div className='flex gap-2 w-fit font-bold text-2xl text-white'>
-             Comming soon
-            </div>
+            <div className='flex w-fit gap-2 text-2xl font-bold text-white'>Comming soon</div>
           </div>
           <Image
             src={wavyAnimation}
             alt='wavy animation'
-            className='absolute z-0 h-full w-full object-cover rotate-180'
+            className='absolute z-0 h-full w-full rotate-180 object-cover'
           />
         </div>
       </section>
